b24deabf02b51f25ef417c7c5b4777f11c2759da
JAkkerman <akkermanjoos@gmail.com>
2019-04-17 17:51:20 +0200

    JA 17-04-2018 (1)

--- Week_2/eda.py
+++ Week_2/eda.py
@@ -17,101 +17,105 @@ GDP_cap = "GDP ($ per capita) dollars"
 
 def cleandata(datafile):
     """
-    cleans the relevant data
+    Cleans data for the following relevant columns:
+    - "Country"
+    - "Region"
+    - "Pop. Density (per sq. mi.)"
+    - "Infant mortality (per 1000 births)"
+    - "GDP ($ per capita) dollars"
     """
 
-    # open csv file and edit data
+    # open csv file, select relevant columns, replace 'unknown' by NaN, replace commas by dots
     data = pd.read_csv(datafile)
-    data.replace('', np.nan)
-
-    num_catergories = [pop_dens, inf_mort, GDP_cap]
-
-    # select only the relevant columns
     data = data[["Country", "Region", pop_dens, inf_mort, GDP_cap]]
+    data = data.replace('unknown', np.nan)
+    data = data.replace(',', '.', regex=True)
 
-    # clean region data (delete excess spaces)
-    for value in data["Region"]:
-        data["Region"] = data["Region"].replace(value, ' '.join(value.split()))
+    # GDP data for Suriname is factually incorrect, therefore it is deleted from the data set
+    data.at[193, GDP_cap] = np.nan
 
-    # clean population density data
-    data[pop_dens] = data[pop_dens].replace('unknown', np.nan)
-    for value in data[pop_dens]:
-        data[pop_dens] = data[pop_dens].replace(value, float(str(value).replace(",", ".")))
+    # clean country and region data of excess spaces
+    data["Country"] = data["Country"].str.strip()
+    data["Region"] = data["Region"].str.strip()
 
-    # clean infant mortality data
-    for value in data[inf_mort]:
-        data[inf_mort] = data[inf_mort].replace(value, float(str(value).replace(",", ".")))
+    # delete dollar addition from GDP per capita data
+    data[GDP_cap] = data[GDP_cap].str.strip(" dollars")
 
-    # clean GDP per capita data
-    data[GDP_cap] = data[GDP_cap].replace('unknown', float(np.nan))
-    for value in data[GDP_cap]:
-        if type(value) == str:
-            data[GDP_cap] = data[GDP_cap].replace(value, int(''.join(i for i in value if i.isdigit())))
+    # change infant mortality, population density and GDP per capita data to floats
+    data[[inf_mort, pop_dens, GDP_cap]] = data[[inf_mort, pop_dens, GDP_cap]].astype(float)
 
     return data
 
 
 def getvalues(data):
     """
-    calculates the mean, median, mode and standard deviation
+    Calculates the mean, median and mode of GDP per capita data and Five Numbers
+    Summary of infant mortality data
     """
 
-    # compute the mean, median and mode for GDP per capita and print
-    mean_GDPCap = data[GDP_cap].mean(skipna=True)
-    median_GDPCap = data[GDP_cap].median(skipna=True)
-    mode_GDPCap = data[GDP_cap].mode()
-    print(f"Mean of GDP per capita: {mean_GDPCap}")
-    print(f"Median of GDP per capita: {median_GDPCap}")
-    print(f"Mode of GDP per capita: {mode_GDPCap}")
+    # compute values for GDP per capita and print
+    print("Mean, Median and Mode of worldwide GDP per capita:")
+    print(f"Mean of GDP per capita: {int(data[GDP_cap].mean(skipna=True))}")
+    print(f"Median of GDP per capita: {data[GDP_cap].median(skipna=True)}")
+    print(f"Mode of GDP per capita: {data[GDP_cap].mode()[0]}")
+    print(f"Standard deviation of GDP per capita: {data[GDP_cap].std()}")
+    print()
 
-    # compute the five number summary for infant mortality and print
-    infmort_Min = data[inf_mort].min()
-    infmort_FQuin = data[inf_mort].quantile([0.25])
-    infmort_Med = data[inf_mort].median()
-    infmort_TQuin = data[inf_mort].quantile([0.75])
-    infmort_Max = data[inf_mort].max()
-    print(f"Minimum amount of child mortality: {infmort_Min}")
-    print(f"First quartile of child mortality: {infmort_FQuin}")
-    print(f"Median of child mortality: {infmort_Med}")
-    print(f"Third quartile of child mortality: {infmort_TQuin}")
-    print(f"Maximum amount of child mortality: {infmort_Max}")
+    # compute values for infant mortality and print
+    print("Five number summary for Infant Mortality Rate per Country:")
+    print(f"Minimum: {data[inf_mort].min()}")
+    print(f"Q1: {data[inf_mort].quantile(q = 0.25)}")
+    print(f"Median: {data[inf_mort].median()}")
+    print(f"Q3: {data[inf_mort].quantile(q = 0.75)}")
+    print(f"Maximum: {data[inf_mort].max()}")
 
 
 def plotGDP(data):
     """
-    plots a histogram showing GDP data
+    Plots a histogram showing GDP per capita data
     """
 
+    data[GDP_cap].plot.hist(bins=100)
     plt.grid()
-    plt.hist(data[GDP_cap], bins = 1000)
     plt.title('Distribution of GDP per capita')
     plt.ylabel('Amount of countries')
     plt.xlabel('GDP per capita')
-    plt.xlim([0, 50000])
-    plt.yticks(range(0, 21))
+    plt.axis([0, 60000, 0, 28])
     plt.show()
 
 
 def plotInfMort(data):
     """
-    plots a boxplot showing infant mortality data
+    Plots a boxplot showing overall infant mortality data and per region
     """
 
-    boxplot = data.boxplot(column = [inf_mort])
+    # plots boxplot of overall infant mortality
+    data.boxplot(column = [inf_mort])
     plt.title("Distribution of Infant Mortality (per 1000 births)")
     plt.ylabel("Infant Mortality")
-    plt.xlabel("")
+
+    plt.show()
+
+    # plots boxplot of infant mortality per region
+    data.boxplot(column=[inf_mort], by="Region")
+    plt.suptitle("")
+    plt.xticks(rotation=80)
+    plt.title("Distribution of Infant Mortality (per 1000 births) per region")
+    plt.ylabel("Infant Mortality")
+
     plt.show()
 
 
 def convert(data):
     """
-    sets up datafile in json format
+    Sets up datafile in json format
     """
+
     data = data.set_index("Country").to_json("data.json", orient='index')
 
 
 if __name__ == "__main__":
+
     # clean data to make it usable
     CLEANED_DATA = cleandata(INPUT_CSV)
 
@@ -121,7 +125,7 @@ if __name__ == "__main__":
     # plot a histogram analyzing GDP data
     plotGDP(CLEANED_DATA)
 
-    # plot a boxplot analyzing infant mortality data
+    # plot boxplots analyzing infant mortality data
     plotInfMort(CLEANED_DATA)
 
     # convert to json file
